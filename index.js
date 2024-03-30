e441e50be8d137e00b0e4f938ef080e3c81cab08
Abdifatah Musa <lawznleytha@gmail.com>
2024-03-30 12:11:36 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31
+let num2 = 2
+let multiply = 31 * 2
 
-// Write your code here
+let random = Math.floor(Math.random() * 10)
+
+let num3 = 8
+let num4 = 2
+let mod = 8/2
+
+let x =Math.max(4,8,12,16,20)
+let max = Math.max(20)
